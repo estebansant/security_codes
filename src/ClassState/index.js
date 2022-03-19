a4180c9b91166c2fbd4ce65b79b3e1f2ef8d5ebe
estebansant <estebansant30@gmail.com>
2022-03-19 15:01:11 -0300

    made Class State work propperly and finished the project

--- src/ClassState/index.js
+++ src/ClassState/index.js
@@ -12,8 +12,10 @@ class ClassState extends React.Component {
 
         this.state = {
             value: '',
-            error: false,
+            error: false, 
             loading: false,
+            deleted: false,
+            confirmed: false,
         }
     }
 
@@ -34,7 +36,7 @@ class ClassState extends React.Component {
 
             setTimeout(() => {
                 if(this.state.value === SECURITY_CODE){
-                    this.setState({error: false, loading: false})
+                    this.setState({error: false, loading: false, confirmed: true})
                 } else {
                     this.setState({ error: true, loading: false })
                 }
@@ -43,42 +45,85 @@ class ClassState extends React.Component {
         }
     }
     
-
     render() {
-        return (
-            <div>
-                <h2>Remove {this.props.name}</h2>
-
-                <p>Please, write your security code to proceed with this operation</p>
-
-
-                {/*Defining what happens when error and loading are true*/}
-
-                {(this.state.error && !this.state.loading) && (
-                    <p>Error: The security code is not correct</p>
-                )}
-
-                {this.state.loading && (
-                    <Loading />
-                )}
-
-                <input
-                    type="text"
-                    placeholder="Security code"
-                    value = {this.state.value}
-                    onChange = {(event) => {
-                        this.setState({value: event.target.value});
-                    }}
-                ></input>
-
-                <button 
-                    type="button"
-                    onClick={() => this.setState({loading: !this.state.loading})}
-                    >
-                        Check</button>
-            </div>
-        )
+        if(!this.state.deleted && !this.state.confirmed){
+                return (
+                    <div>
+                        <h2>Remove {this.props.name}</h2>
+        
+                        <p>Please, write your security code to proceed with this operation</p>
+        
+        
+                        {/*Defining what happens when error and loading are true*/}
+        
+                        {(this.state.error && !this.state.loading) && (
+                            <p>Error: The security code is not correct</p>
+                        )}
+        
+                        {this.state.loading && (
+                            <Loading />
+                        )}
+        
+                        <input
+                            type="text"
+                            placeholder="Security code"
+                            disabled={this.state.loading}
+                            value = {this.state.value}
+                            onChange = {(event) => {
+                                this.setState({value: event.target.value});
+                            }}
+                        ></input>
+        
+                        <button 
+                            type="button"
+                            onClick={() => this.setState({loading: !this.state.loading})}
+                            >
+                                Check</button>
+                    </div>
+                )
+            
+            } else if (this.state.confirmed && !this.state.deleted){
+                return(
+                    <React.Fragment>
+                        <p>Please confirm your action. Are you sure you want to delete {this.props.name}?</p>
+                        <button
+                            type="button"
+                            onClick={() => {
+                                this.setState({deleted: true,})
+                            }}
+                        >Yes, just delete it</button>
+                        <button
+                            type="button"
+                            onClick={() => {
+                                this.setState({
+                                    confirmed: false,
+                                    value: '',
+                                })
+                            }}
+                        >Nah, I changed my mind</button>
+                    </React.Fragment>
+                );
+            } else {
+                return(
+                    <React.Fragment>
+                        <p>Succesfully deleted</p>
+                        <button
+                            type="button"
+                            onClick={() => {
+                                this.setState({
+                                    confirmed: false,
+                                    deleted: false,
+                                    value: '',
+                                })
+                            }}
+                        >
+                            Reset, undo the changes
+                        </button>
+                    </React.Fragment>
+                );
+            }
     }
+    
 }
 
 export { ClassState };
